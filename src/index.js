837ee8a5a04eb61cc03c04c87356dbca73c636a0
lauramayc <lauramayc@live.com>
2024-08-25 19:40:43 +0100

    tidy up

--- src/index.js
+++ src/index.js
@@ -4,15 +4,33 @@ setTimeout(() => {
         delay: 200,
         fallSpeed: 2,
     });
-}, 1000);   
+}, 1000);  
 
+function changeTemp (response) { 
+    
+   let tempElement = document.querySelector("#temp-number"); 
+ let responseData = response.data.temperature.current;
+   tempElement.innerHTML = ` ${Math.round(responseData)}` ;
+   let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.city;
+}
 
-function changeCity(event) { 
+function searchCity(city) { 
+    let apiKey = `a6ee8503c8fee7b4f4c3ocbtac96961d`
+   let url =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(url) .then(changeTemp);
+}
+
+function changeCity(event) {    
     event.preventDefault(); 
     let searchInput = document.querySelector("#search-form-input"); 
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+   
+    searchCity(searchInput.value);
+   
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCity);
+
+searchCity("Tokyo");
